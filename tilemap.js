fe0badddc62ba03896ec7ea1ba64c7c4f1f29484
Douglas Gregory <dmgregory+git@gmail.com>
2021-12-08 16:13:43 -0500

    Tilemap comments

--- tilemap.js
+++ tilemap.js
@@ -20,11 +20,17 @@ Object.freeze(Tile);
 // Gathers up SVG images from the HTML document into images we can
 // stamp into our canvas, keyed by members of the Tile enumeration.
 class TileLibrary {
-    #tileImages = [];
-    #remainingToLoad = 0;
-    #tileSize = 0;
+    /** @type {Object} Map of HTMLImageElements representing each tile. */
+    #tileImages = {};
+    /** @type {Number} Number of SVG images we have not yet finished loading into usable tiles. */
+    #remainingToLoad = -1;
+    /** @type {Number} Pixel width of a standard tile. */
+    #tileSize = -1;
 
-    // Accepts a function to call when all tiles have finished loading.
+    /**
+     * Create a new TileLibrary by trawling the HTML document for all SVG tags.
+     * @param {ActionCallback} onLoaded Function to call when loading completes.
+     */
     constructor(onLoaded) {
         // Gather all SVG elements in the page, and record them as waiting to load.
         const svgs = document.getElementsByTagName("svg");
@@ -33,9 +39,11 @@ class TileLibrary {
         // Track loading progress, and call the callback once all have loaded.
         // Using arrow function syntax to automatically bind "this" to this context.
         let onTileLoad = () => {
+            // Once we've loaded the last tile, set our tile size and call the
+            // onLoaded callback, if we were given one.
             if (--this.#remainingToLoad == 0) {
                 // HACK: Pick one tile arbitrarily to set the size.
-                this.#tileSize = this.#tileImages[Tile.DIRT_BLOCK].width;
+                this.#tileSize = this.#tileImages[Tile.DIRT_BLOCK].width;                
                 if (onLoaded) onLoaded();
             }
         };
@@ -81,12 +89,18 @@ class TileLibrary {
 // Represents a whole platformer map, or a piece of one,
 // as a 2D array of tile symbols.
 class MapChunk {
+    /** @type {string[][]|Symbol[][]} Array of arrays of tile symbols in the map.*/
     #columns = [];
 
+    /** @type {boolean} Flag controlling whether string data in tilemap should be drawn.*/
     printText = true;
 
-    // Accepts the width and height of the map/chunk as integers,
-    // and an optional tile symbol to fill it with initially.
+    /**
+     * Build a new map chunk with the given dimensions / initial content.
+     * @param {Number} columnCount Width of the map, in tiles.
+     * @param {Number} rowCount Height of the map, in tiles.
+     * @param {Symbol?} fillWith Optional: fill map with this symbol. Uses Tile.NONE if absent.
+     */    
     constructor(columnCount, rowCount, fillWith) {
         // Default to fill with the empty tile if no other value is provided.
         if (!fillWith) fillWith = Tile.NONE;
@@ -102,27 +116,51 @@ class MapChunk {
         }
     }
 
-    // Quick method to get the width and height of a map chunk.
+    /**
+     * Structure representing the width and height of a tile map.
+     * @typedef {Object} TileDimensions
+     * @property {Number} columns
+     * @property {Number} rows
+     */
+
+    /** 
+     * @returns {TileDimensions} Width and height of this tilemap.
+     */
     getDimensions() {
         return { columns: this.#columns.length, rows: this.#columns[0].length };
     }
 
-    // Place just a single tile, and skip it if it's out of bounds.
-    // (This is different from "fill", which clamps the rectangle within bounds)
-    // Returns "this" so calls can be chained.
+    /**
+     * Place just a single tile, and skip it if it's out of bounds.
+     * (This is different from "fill", which clamps the rectangle within bounds)
+     * @param {Symbol|string} tile Tile to place
+     * @param {*} x Horizontal coordinate (0 is the leftmost column)
+     * @param {*} y Vertical coordinate (0 is the top row)
+     * @returns {MapChunk} Reference to self, so calls can be chained.
+     */
     place(tile, x, y) {
         const { columns, rows } = this.getDimensions();
         if (x >= 0 && x < columns && y >= 0 && y < rows) this.#columns[x][y] = tile;
         return this;
     }
 
-    // Fills a rectangular region of the map from (xMin, yMin) up to (xMax, yMax) (inclusive).
-    // Portions of the rectangle hanging outside the map bounds are ignored.
-    // Coordinates are 0-based, with 0, 0 being the top-left corner.
-    // If the max is omitted, the corresponding min is used instead.
-    // The tile argument can be a single tile symbol, or a function accepting an (x, y) coordinate
-    // and returning a tile symbol to place at that location.
-    // Returns "this" so calls can be chained.
+    /**
+     * Callback for filling a map chunk with procedurally-selected tiles.
+     * @callback TileSelector
+     * @param {Number} x Horizontal coordinate of tile to be placed.
+     * @param {Number} y Vertical coordinate of tile to be placed.
+     * @returns {Symbol|string} Tile to place at this position.
+     */
+
+    /**
+     * Fills a rectangular region of the map.
+     * @param {Symbol|string|TileSelector} tile Tile to fill with, or a callback to select a distinct tile at each x, y.
+     * @param {Number} xMin Leftmost column to fill - clamped to zero if less.
+     * @param {Number} yMin Topmost row to fill - clamped to zero if less.
+     * @param {Number} xMax Rightmost column to fill - clamped within map bounds if outside.
+     * @param {Number} yMax Bottommost row to fill - clamped within map bounds if outside.
+     * @returns {MapChunk} Reference to self, so calls can be chained.
+     */
     fill(tile, xMin, yMin, xMax, yMax) {
         const { columns, rows } = this.getDimensions();
 
@@ -147,18 +185,24 @@ class MapChunk {
         // Iterate over the rectangle and set the tile symbol in each location.
         for (let x = xMin; x <= xMax; x++) {
             for (let y = yMin; y <= yMax; y++) {
-                let tileToPlace = tile;
-                if (typeof tile === "function") tileToPlace = tile(x, y);
-
-                this.#columns[x][y] = tileToPlace;
+                if (typeof tile === "function")  {
+                    this.#columns[x][y] = tile(x, y); 
+                } else {
+                    this.#columns[x][y] = tile;
+                }
             }
         }
 
         return this;
     }
 
-    // Returns the tile value at the given x, y coordinates.
-    // Accesses outside the map bounds return NONE.
+
+    /**
+     * Returns the tile value at the given x, y coordinates.
+     * @param {Number} x Horizontal coordinate of tile (0 = leftmost column).
+     * @param {Number} y Vertical coordinate of tile (0 = top row).
+     * @returns {Symbol|string} Tile at given coordinates, or Tile.NONE if out of bounds.
+     */
     getTileAt(x, y) {
         const { columns, rows } = this.getDimensions();
         if (x < 0 || x >= columns) return Tile.NONE;
@@ -166,10 +210,21 @@ class MapChunk {
         return this.#columns[x][y];
     }
 
-    // Draws the contents of the map chunk into the provided canvas 2D context ctx
-    // using the images in the TileLibrary tiles. Drawing can be limited to a rectangle
-    // rect (x, y, width, height), or to the whole canvas area if rect is omitted.
-    // The map is automatically scaled uniformly to fit the available drawing area.
+    /**
+     * Data structure for determining a subsection of a canvas to draw into.
+     * @typedef Rect
+     * @param {Number} x Left edge of rectangle, in pixels.
+     * @param {Number} y Top edge of rectangle, in pixels.
+     * @param {Number} width Width of rectangle, in pixels.
+     * @param {Number} height Height of rectangle, in pixels.
+     */
+   
+    /**
+     * Draws the contents of the map chunk into the provided canvas 2D context.
+     * @param {CanvasRenderingContext2D} ctx Context to draw into.
+     * @param {TileLibrary} tiles Library of tile images to use.
+     * @param {Rect?} rect Portion of the canvas to draw into. Defaults to whole canvas if absent.
+     */
     draw(ctx, tiles, rect) {
         // Default to the whole canvas if a rect was not provided.
         if (!rect)
@@ -242,10 +297,15 @@ class MapChunk {
         ctx.restore();
     }
 
-    // Places the selected tile symbol in the column indexed by x (leftmost column is 0)
-    // in the first non-solid gap above a solid tile. If there are no solid tiles in the column,
-    // it adds one at the bottom. If there is no gap, it adds one at the top.
-    // Used the guarantee placing the start / end markers.
+    /**
+     * Places the selected tile symbol in the specified column,
+     * in the first non-solid gap above a solid tile. If there are no solid tiles in the column,
+     * it adds one at the bottom. If there is no gap, it adds one at the top.
+     * Used the guarantee placing the start / end markers.
+     * @param {Symbol|string} tileToPlace What tile symbol to record.
+     * @param {Number} x Column to place into (0 = leftmost column).
+     * @returns {MapChunk} Reference to self, so calls can be chained.
+     */
     placeAtopGround(tileToPlace, x) {
         // Clamp the index into the range of valid columns.
         x = Math.max(0, Math.min(x, this.#columns.length - 1));
@@ -283,17 +343,26 @@ class MapChunk {
             column[column.length - 1] = Tile.SOLID;
             column[column.length - 2] = tileToPlace;
         }
+
+        return this;
     }
 
-    // Places a start marker in the first column of the map,
-    // and an end marker in the last column.
+    /**
+     * Places a start marker in the first column of the map,
+     * and an end marker in the last column.
+     */
     prepStartEnd() {
         this.placeAtopGround(Tile.START_SIGN, 0);
         this.placeAtopGround(Tile.GREEN_FLAG, this.#columns.length - 1);
     }
 
-    // Copies the contents of this MapChunk into another one (map), at a given (x, y) offset.
-    // Portions of this MapChunk that overhang the bounds of the destination map are ignored.
+    /**
+     * Copies the contents of this MapChunk into another one, at a given (x, y) offset.
+     * Portions of this MapChunk that overhang the bounds of the destination map are ignored.
+     * @param {MapChunk} map Destination map chunk to copy this one into, overwriting what's there.
+     * @param {Number} startX Leftmost column to write into.
+     * @param {Number} startY Top row to write into.
+     */
     stampInto(map, startX, startY) {
         const { columns, rows } = map.getDimensions();
 
@@ -312,27 +381,35 @@ class MapChunk {
         }
     }
 
+    /**
+     * Checks whether the given coordinates contain a solid tile.
+     * @param {Number} x Column to check (0 = leftmost column).
+     * @param {Number} y Row to check (0 = top row).
+     * @returns {boolean} True if there is a solid tile at the given coordinates.
+     */
     isSolid(x, y) {
         const tile = this.getTileAt(x, y);
         switch(tile) {
             case Tile.SOLID:
-            case Tile.EXCLAMATION_BOX:
             case Tile.EXCLAMATION_BOX:
                 return true;
             default:
                 return false;
         }
     }
-
-    // Empties this map, replacing all contents with the "None" tile symbol.
+    
+    /**
+     * Empties this map, replacing all contents with the "None" tile symbol.
+     */
     clear() {
         const { columns, rows } = this.getDimensions();
         this.fill(Tile.NONE, 0, 0, columns, rows);
+   }
 
-        // "path" field is glued on by the Path-Based generator. Clear it too.
-        this.path = undefined;
-    }
-
+    /**
+     * Get the aspect ratio of this map chunk.
+     * @returns {Number} Ratio of height:width of this map.
+     */
     aspect() {
         const { columns, rows } = this.getDimensions();
         return rows/columns;
